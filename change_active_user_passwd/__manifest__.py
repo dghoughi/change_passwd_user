d87c9422e2f5118a8f1006631bcadcd7ad83319d
dgh <dghoughi2403@gmail.com>
2018-01-10 09:31:48 +0000

    11

--- change_active_user_passwd/__manifest__.py
+++ change_active_user_passwd/__manifest__.py
@@ -4,10 +4,6 @@
 
     'summary': """
         this module allow any user to update his personal password without having Access Rights .
-        
-        this is my first module in odoo apps please if any one find it useful 
-        or any ideas to add please let me know by email  'dghoughi2403@gmail.com' 
-        this will encorage me to make more 
         """,
 
     'description': """
