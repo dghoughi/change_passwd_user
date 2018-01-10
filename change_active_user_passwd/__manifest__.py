1d6b4970cad8b66e4090870b95779814ad8c9498
dgh <dghoughi2403@gmail.com>
2018-01-10 08:45:28 +0000

    first commit

---
+++ change_active_user_passwd/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "change active user passwd",
+
+    'summary': """
+        this module allow any user to update his personal password without having Access Rights .
+        
+        this is my first module in odoo apps please if any one find it useful 
+        or any ideas to add please let me know by email  'dghoughi2403@gmail.com' 
+        this will encorage me to make more 
+        """,
+
+    'description': """
+       this module allow any user to update his personal password without having Access Rights .
+       
+        this is my first module in odoo apps please if any one find it useful 
+        or any ideas to add please let me know by email  'dghoughi2403@gmail.com' 
+        this will encorage me to make more 
+    """,
+
+    'author': "Marocsys",
+    'website': "http://www.marocsys.com",
+
+    'category': 'Human Resources',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        'security/change_passwd_security.xml',
+        'views/change_passwd.xml',
+    ],
+}
